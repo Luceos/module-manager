f21e8075f832e4a9db41d34ffb34f995b52104bf
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-07-09 22:38:58 +0200

    first steps

---
+++ src/ModuleManagerServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace LaraLeague\ModuleManager;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ModuleManagerServiceProvider
+ * @package LaraLeague\ModuleManager
+ */
+class ModuleManagerServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
